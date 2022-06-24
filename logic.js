7eff21b7313fa318300a3e1d03cf9474b9ff35d9
Griffin <griffinlebeau2@gmail.com>
2022-06-24 11:19:45 -0500

    overhaul

--- logic.js
+++ logic.js
@@ -1,87 +1,98 @@
-var questionArray = []
 
-var questionOne = {
-    question: "question",
-    correct: "correct",
-    incorrectOne: "incorrect",
-    incorrectTwo: "incorrect",
-    incorrectThree: "incorrect",
-}
-
-var questionTwo = {
-    question: "question",
-    correct: "correct",
-    incorrectOne: "incorrect",
-    incorrectTwo: "incorrect",
-    incorrectThree: "incorrect",
-}
-
-var questionThree = {
-    question: "question",
-    correct: "correct",
-    incorrectOne: "incorrect",
-    incorrectTwo: "incorrect",
-    incorrectThree: "incorrect",
-}
-
-var questionFour = {
-    question: "question",
-    correct: "correct",
-    incorrectOne: "incorrect",
-    incorrectTwo: "incorrect",
-    incorrectThree: "incorrect",
-}
-
-var questionFive = {
-    question: "question",
-    correct: "correct",
-    incorrectOne: "incorrect",
-    incorrectTwo: "incorrect",
-    incorrectThree: "incorrect",
-}
-
-questionArray[0] = questionOne;
-questionArray[1] = questionTwo;
-questionArray[2] = questionThree;
-questionArray[3] = questionFour;
-questionArray[4] = questionFive;
-
-console.log(questionOne.correct);
-
-var shuffle = function (questionArray) {
-    let currentIndex = questionArray.length, randomIndex;
-    while (currentIndex !=0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex--;
-        [questionArray[currentIndex], questionArray[randomIndex]] = 
-        [questionArray[randomIndex], questionArray[currentIndex]];
+var questionArray = [
+    {
+        question: "Which character in an IF statement is used to check if 2 things are NOT EQUAL",
+        a: "!",
+        b: "?",
+        c: "$",
+        correctAnswer: "a"
     }
-    return questionArray;
+]
+
+var scores = []
+
+
+
+var generateQuestionEl = function(questionArray) {
+    var main = $("<div>").addClass("quiz-container")
+    var question = $("<h5>").text(questionArray[i].question)
+    var answerA = $("<p>").text(questionArray[i].a).attr("id", "a");
+    var answerB = $("<p>").text(questionArray[i].b).attr("id", "b");
+    var answerC = $("<p>").text(questionArray[i].c).attr("id", "c");
+    $(".main-container").append(main);
+    main.append(question);
+    main.append(answerA);
+    main.append(answerB);
+    main.append(answerC);
 }
 
-var startQuiz = function(questionArray) {
-    //declare and remove intro elements
-    var introJumbo = document.getElementById("#intro-h3");
-    introJumbo.remove;
-    var introText = document.getElementById("#intro-p");
-    introText.remove;
-    //create question text and answer option buttons
-    var questionText = document.createElement("h2");
-    var introCard = document.getElementById("#intro-card");
-        introCard.appendChild(questionText);
-    var buttonOne = document.createElement("button");
-        buttonOne.setAttribute("id", "buttonOne");
-        introCard.appendChild(buttonOne);
-    var buttonTwo = document.createElement("button");
-        buttonTwo.setAttribute("id", "buttonTwo");
-        introCard.appendChild(buttonTwo);
-    var buttonThree = document.createElement("button");
-        buttonThree.setAttribute("id", "buttonThree");
-        introCard.appendChild(buttonThree);
-    var buttonFour = document.createElement("button");
-        buttonFour.setAttribute("id", "buttonFour");
-        introCard.appendChild(buttonFour);
+var endQuiz = function() {
+    $(".quiz-container").addClass("is-hidden");
+    var scoreForm = $("<form>");
+    var scoreFormLabel = $("<label>")
+                        .attr("for", "initials")
+                        .text("Initials:")
+    var scoreFormInput = $("<input>")
+                        .attr("type", "text")
+                        .attr("id", "initials")
+                        .attr("name", "initials")
+    var scoreFormSubmit = $("<input>")
+                        .attr("type", "submit")
+                        .attr("value", "Submit")
+    scoreForm.append(scoreFormLabel);
+    scoreForm.append(scoreFormInput);
+    scoreForm.append(scoreFormSubmit);
+    $(".main-container").append(scoreForm);
+    scoreFormSubmit.on("submit", function() {
+        var scoreTemp = {initials:"", score:""}
+        scoreTemp.initials = scoreFormInput.text().trim();
+        scoreTemp.score = count;
+        scores.push(scoreTemp);
+        saveScores(scores);
 
-    
-
+    })
 }
+
+var displayScores = function() {
+    scoreForm.addClass("is-hidden");
+    var scoresList = $("<ol>")
+    $(".main-container").append(scoresList);
+    localStorage.getItem("scores");
+    for (i = 0; i< scores.length; i++){
+        var scoreItem = $("<li>").text(scores[i].initials + "" + scores[i].score);
+        scoresList.append(scoreItem);
+    }
+}
+
+$(".start-btn").on("click", function() {
+    $(".intro-container").addClass("is-hidden");
+    var count = 75
+    var timer = setInterval(function() {
+        $("#counter").html(count--);
+        if(count == 1) clearInterval(timer);
+    }, 1000);
+    while (count > 1) {
+        for (i = 0; i < questionArray.length; i++) {
+            generateQuestionEl(questionArray[i]);
+            $(".answer-container").on("click", "p", function() {
+                if ($(this).attr("id") === questionArray[i].correctAnswer) {
+                    $(this).addClass("correct")
+                    return
+                }
+                else {
+                    $(this).addClass("incorrect");
+                    count - 10
+                    return
+            
+                }
+            })
+        }
+        endQuiz(count)
+    }
+    endQuiz(count)
+
+})
+
+
+
+
